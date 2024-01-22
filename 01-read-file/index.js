96748d8ebb5f9e068532a51b4aa10e86c2c96d8e
tmkvch18 <tomkovich18@gmail.com>
2024-01-22 22:55:58 +0300

    feat: add solution for task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const readStream = fs.createReadStream(
+  path.resolve(__dirname, 'text.txt'),
+  'utf-8',
+);
+
+let data = '';
+readStream.on('data', (chunk) => (data += chunk));
+readStream.on('end', () => console.log(data));
